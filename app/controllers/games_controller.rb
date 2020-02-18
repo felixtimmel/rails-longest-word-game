1d67fac15f4ae447d8460c9216d7fb321e360034
Felix Timmel <f.timmel95@gmail.com>
2020-02-17 21:15:57 -0500

    style modif

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -19,25 +19,27 @@ class GamesController < ApplicationController
     language = JSON.parse(language_word)
     if language['found'] == false
       @message = "Sorry but #{@word} does not seem to be a valid English word..."
-    elsif in_grid(@word, @letter_string) == false
+    elsif include?(@word, @letter_string) == false
       @message = "Sorry but #{@word} can't be built out of #{@letter_string}"
     else
       @message = "Congratulations! #{@word} is a valid English word"
     end
   end
 
-  def in_grid(attempt, grid)
-    unused_grid = grid.clone
-    attempt.upcase.chars.each do |letter|
-      if unused_grid.include? letter
-        unused_grid.chars.delete_at(unused_grid.index(letter))
-      else
-        return false
-      end
-    end
-    return true
-  end
-  # def include?(word, letters)
-  #   word.upcase.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  private
+
+  # def in_grid(attempt, grid)
+  #   unused_grid = grid.clone
+  #   attempt.upcase.chars.each do |letter|
+  #     if unused_grid.include? letter
+  #       unused_grid.chars.delete_at(unused_grid.index(letter))
+  #     else
+  #       return false
+  #     end
+  #   end
+  #   return true
   # end
+  def include?(word, letters)
+    word.upcase.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
 end
